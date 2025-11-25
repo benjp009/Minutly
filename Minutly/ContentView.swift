a893eb89de29e1039fd1c36dd3fb888ba3b5027f
Benjamin Patin <patin.benjamin@gmail.com>
2025-11-25 16:07:02 +0100

    Initial Commit

---
+++ Minutly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Minutly
+//
+//  Created by Benjamin Patin on 25/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Minutly/MinutlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinutlyApp.swift
+//  Minutly
+//
+//  Created by Benjamin Patin on 25/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct MinutlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
